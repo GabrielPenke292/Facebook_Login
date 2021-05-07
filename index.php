843e47ad512b5659337b601672ab208f443a94ee
Gabriel Penke <gabri.penke@gmail.com>
2021-05-07 19:07:46 -0300

    Novos itens foram acrescentados
    Foram acrescentados:
     - formulário de login
     - formulário de cadastro
     - imagem de background principal
     - Estilização no css

--- index.php
+++ index.php
@@ -36,5 +36,42 @@
             <div class="clear"></div>
         </div> <!-- center -->
     </header>
+
+    <section id="main">
+        <div class="center">
+            <div class="img-pessoas">
+                <img src="img1.png" alt="imagem de fundo da main">
+            </div><!-- img pessoas-->
+            <div class="abrir-conta">
+                <h2>Abra uma conta</h2>
+                <h3>É gratuito e sempre será.</h3>
+                <form action="" class="create-account">
+                    <div class="w50">
+                        <input type="text" placeholder="Nome">
+                    </div><!-- w50 -->
+
+                    <div class="w50">
+                        <input type="text" placeholder="Sobrenome">
+                    </div><!-- w50 -->
+
+                    <div class="w100">
+                        <input type="email" placeholder="Email">
+                    </div><!--w100-->
+
+                    <div class="w100">
+                        <input type="password" placeholder="Senha">
+                    </div><!--w100-->
+                    
+                    <div class="w100">
+                        <input type="submit" name="acao" value="Cadastrar">
+                    </div><!-- w100 -->
+                    
+                    <div class="clear"></div>
+                </form><!-- create account -->
+            </div><!-- abrir conta -->
+            <div class="clear"></div>
+        </div><!-- center -->
+    </section><!-- main -->
+
 </body>
 </html>
