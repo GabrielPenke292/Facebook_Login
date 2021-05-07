e7495ceb143ecce7e1bd4aa134f8876f22648f54
Gabriel Penke <gabri.penke@gmail.com>
2021-05-07 17:56:19 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Facebook</title>
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="center">
+            <div class="logo">
+                <h2>Facebook</h2>
+            </div><!-- logo -->
+
+            <form method="post" class="form-login">
+                <div class="form-element">
+                    <p>E-mail ou telefone</p>
+                    <input type="email" name="email" id="email" >
+                </div><!-- form element -->
+                <div class="form-element">
+                    <p>Senha</p>
+                    <input type="password" name="senha" id="senha" >
+                </div><!-- form element -->
+                <div class="form-element">
+                    <input type="submit" name="acao" value="Enviar" >
+                </div><!-- form element -->
+            </form><!-- form login -->
+            <div class="clear"></div>
+        </div> <!-- center -->
+    </header>
+</body>
+</html>
